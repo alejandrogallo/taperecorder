d57c32b4e29e14a26e3d01ad811202bc1c5239a2
Alejandro Gallo <aamsgallo@gmail.com>
2016-12-26 17:56:07 +0100

    add getopt functionality with command flag

--- src/main.c
+++ src/main.c
@@ -14,8 +14,65 @@
 
 
 int
-main()
+main(int argc, char **argv)
 {
+
+  /*char command[MAX_SIZE_CMD];*/
+  const char __OPTIONS[] = "hvVc:";
+  int choice;
+  int option_index = 0;
+  static struct option long_options[] =
+  {
+    /* Argument styles: no_argument, required_argument, optional_argument */
+    {"verbose", no_argument, 0, 'V'},
+    {"version", no_argument, 0, 'v'},
+    {"help", no_argument, 0, 'h'},
+    {"command", required_argument, 0, 'c'},
+    {0,0,0,0}
+  };
+
+
+  while (1)
+  {
+    choice = getopt_long( argc, argv, __OPTIONS,
+        long_options, &option_index);
+    if (choice == -1)
+      break;
+    switch( choice )
+    {
+      case 'v':
+        printf("%s -- Version %s", PACKAGE, VERSION);
+        exit(0);
+        break;
+      case 'h':
+        printf("Usage: %s "
+            "[-h|--help] "
+            "[-v|--version] "
+            "[-V|--verbose] "
+            "[-c|--command <command>] "
+            "\n",PACKAGE);
+        printf("\n  %s\n",PACKAGE_DESCRIPTION);
+        printf("\n");
+        printf("  Options:\n"
+               "    -h|--help       Display this message and exit.\n"
+               "    -v|--version    Display version and exit.\n"
+               "    -V|--verbose    Be verbose in the output.\n"
+               "    -c|--command    Execute a command.\n"
+               "\n"
+               "This program is maintained by Alejandro Gallo\n");
+        exit(0);
+        break;
+      case 'c':
+        run_command(optarg);
+        break;
+      case '?':
+        /* getopt_long will have already printed an error */
+        break;
+      default:
+        /* Not sure how to get here... */
+        return EXIT_FAILURE;
+    }
+  }
 
   mkconfig_dir();
 
@@ -25,5 +82,7 @@ main()
 
 }
 
+/* vim-run: make && ./src/taperecorder --help  */
+/* vim-run: make && ./src/taperecorder --version  */
 /* vim-run: make && ./src/taperecorder  */
 /* vim-run: make CFLAGS=-DDEBUG && ./src/taperecorder  */
